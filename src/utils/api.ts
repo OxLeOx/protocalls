be8f7e3b58dc169cff4eafe350aa24df88dc21a3
LeO <leoh.laxon14@gmail.com>
2024-04-30 21:23:35 -0400

    init

---
+++ src/utils/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+export const getData = async () => {
+  try {
+    const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+      headers: {
+        'X-CMC_PRO_API_KEY': process.env.REACT_APP_COINMARKETCAP_API_KEY,
+      },
+    });
+
+    return response.data.data
+  } catch (err) {
+    console.log(err)
+  }
+}
